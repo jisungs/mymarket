ec31b3982f42f26f294b774dc8ee301aabcbb1a8
jisung <jisung@The-Air.local>
2020-10-16 12:08:00 +0900

    Add templates to base directory

--- mymarket/urls.py
+++ mymarket/urls.py
@@ -16,14 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from products import views
+from products.views import (
+    home_view,
+    product_detail_view,
+    product_list_view,
+    product_api_detail_view,
+)
 
 urlpatterns = [
 
-    path('search/', views.home_view ),
-    path('products/<int:id>/',views.product_detail_view),
+    path('search/', home_view ),
+    path('products/', product_list_view),
+    path('products/<int:pk>/', product_detail_view),
     # path('products/1/',views.product_detail_view),
-    path('api/products/1/', views.product_api_detail_view),
+    path('api/products/1/', product_api_detail_view),
     path('admin/', admin.site.urls),
     # path('market/', include('market.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -4,16 +4,23 @@ from django.http import HttpResponse, JsonResponse,Http404
 from .models import Product
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello world</h1>')
+    # return HttpResponse('<h1>Hello world</h1>')
+    context = {"name":"jisung"}
+    return render(request, "home.html", context)
 
-def product_detail_view(request,id):
-    obj = Product.objects.get(id=id)
+def product_detail_view(request,pk):
+    # obj = Product.objects.get(pk=pk)
     try : 
-        obj = Product.objects.get(id=id)
+        obj = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
         raise Http404
-    return HttpResponse(f"<h1>Product title {obj.title}</h1>")
+    return render(request, "products/detail.html", {"object": obj})
 
-def product_api_detail_view(request, *args, **kwargs):
-    obj = Product.objects.get(id=1)
+def product_list_view(request, *args, **kwargs):
+    qs = Product.objects.all()
+    context = {"object_list": qs}
+    return render(request, "products/list.html", context)
+
+def product_api_detail_view(request,pk, *args, **kwargs):
+    obj = Product.objects.get(pk=pk)
     return JsonResponse({"id:": obj.id})
