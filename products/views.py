110e47cdf49cec43ef9ead8846a0c1994fd4f6d0
jisung <jisung@The-Air.local>
2020-10-16 15:43:03 +0900

    Send data to server using html form

--- products/views.py
+++ products/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse,Http404
 
 from .models import Product
+
+from .form import ProductForm
 # Create your views here.
-def home_view(request, *args, **kwargs):
+def search_view(request, *args, **kwargs):
     # return HttpResponse('<h1>Hello world</h1>')
-    context = {"name":"jisung"}
+    query = request.GET.get('q')
+    qs = Product.objects.filter(title__icontains = query[0])
+    print(qs)
+    context = {"name":"jisung", "query": query}
     return render(request, "home.html", context)
+
+def product_create_view(request, *args, **kwargs):
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == "POST":
+        post_data = request.POST or None
+        if post_data != None:
+            my_form = ProductForm(request.POST)
+            if my_form.is_valid():
+                print(my_form.cleaned_data.get("title"))
+                title_form_input = my_form.cleaned_data.get("title")
+                Product.objects.create(title = title_form_input)
+            # print(my_form.is_valid())
+            # print("post_data", post_data)
+
+    return render(request, "form.html", {})
+
 
 def product_detail_view(request,pk):
     # obj = Product.objects.get(pk=pk)
