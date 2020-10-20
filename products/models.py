a84328eb24105c99aa20a3a2889fbc67d552239a
jisung <jisung@The-Air.local>
2020-10-21 06:53:04 +0900

    Add order app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+
+from decimal import Decimal
 from django.db import models
 from django.conf import settings
 # Create your models here.
@@ -10,6 +12,11 @@ class Product(models.Model):
     title = models.CharField(max_length=220)
     subtitle = models.TextField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
     discountPrice = models.IntegerField(default=0)
+    inventory = models.IntegerField(default=0)
+    featured = models.BooleanField(default=False)
+
+    def has_inventory(self):
+        return self.inventory > 0 #True or false 
 
